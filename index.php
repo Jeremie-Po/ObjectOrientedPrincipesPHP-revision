aa5ab7e5c4edbbf7a2e10d5350cca56ec938ed24
Jérémie P <jpourageaux@hotmail.fr>
2025-01-23 12:07:28 +0100

    S3 storage on index

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <?php
 
+use App\S3Storage;
+use Aws\S3\S3Client;
+use Dotenv\Dotenv;
+
 require 'vendor/autoload.php';
 
-use App\localStorage;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-$storage = new localStorage();
+$s3Key = $_ENV['S3_KEY'];
+$s3Secret = $_ENV['S3_SECRET'];
+
+// Instantiate an Amazon S3 client.
+$client = new S3Client([
+    'version' => 'latest',
+    'region' => 'us-east-1',
+    'credentials' => [
+        'key' => $s3Key,
+        'secret' => $s3Secret,
+    ],
+]);
+
+
+$storage = new S3Storage($client, $_ENV['S3_BUCKET']);
 $storage->put('file.txt', 'Hello, world!');
 
 echo 'done';
--- src/S3Storage.php
+++ src/S3Storage.php
@@ -2,10 +2,28 @@
 
 namespace App;
 
+use Aws\Exception\AwsException;
+use Aws\S3\S3Client;
+
 class S3Storage implements FileStorage
 {
+    public function __construct(protected S3Client $client, string $bucket)
+    {
+    }
+
     public function put($path, $content)
     {
-
+// Upload a publicly accessible file. The file size and type are determined by the SDK.
+        try {
+            $this->client->putObject([
+                'Bucket' => $this->bucket,
+                'Key' => $path,
+                'Body' => $content,
+            ]);
+        } catch (AwsException $e) {
+            echo "There was an error uploading the file: ".$e->getAwsErrorMessage()."\n";
+            echo "AWS Error Code: ".$e->getAwsErrorCode()."\n";
+            echo "Request ID: ".$e->getRequestId()."\n";
+        }
     }
 }
